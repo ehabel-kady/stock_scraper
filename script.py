61dfd53526a2ab1d4aa6038632089d0df3bee318
ehabel-kady <ehabadelkady@gmail.com>
2021-02-22 02:49:22 +0300

    added zacks url and headers

--- script.py
+++ script.py
@@ -96,3 +96,22 @@ try:
 except IOError:
     print("I/O error")
 # curl 'https://stockinvest.us/list/buy/top100' \
+
+url_zacks = 'https://www.zacks.com/esp/esp_buysell_data_handler.php'
+headers_zacks = {
+    'Accept': 'application/json, text/javascript, */*; q=0.01',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'en-US,en;q=0.9,ar-EG;q=0.8,ar;q=0.7',
+    'Connection': 'keep-alive',
+    'Content-Length': '13',
+    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+    'Cookie': 'user_session=9acaa4926660df613307834b6540852f; AMCVS_3064401053DB594D0A490D4C%40AdobeOrg=1; _ga=GA1.2.867734770.1613669457; s_cc=true; _fbp=fb.1.1613669457116.1705067711; _vwo_uuid_v2=D64E30872A277B9ABDE2884A327CB184A|2f92e2894e6f2ac197a6c00c479bebfc; _vis_opt_s=1%7C; _vis_opt_test_cookie=1; _vwo_uuid=D64E30872A277B9ABDE2884A327CB184A; _vwo_ds=3%241613669456%3A49.01995459%3A%3A; com.silverpop.iMAWebCookie=53605809-7e06-cddc-44b3-424ac9103612; cf60519feb1344e434b8444b746a915b=cdaeeeba9b4a4c5ebf042c0215a7bb0e; _gid=GA1.2.445744564.1613905398; report_name=7 Best Stocks for the Next 30 Days; welcome_back=1; trustedsite_visit=1; s_prdcts=%5B%5B%2735049%27%2C%271613905975776%27%5D%5D; HomeTradeBanner=1; s_v17=zp_prempage_beatearnings_espfilter; funnelCookie=premiumtrader; remember_me=1; CUSTOMER_ID=772270044; ZTOKENID=df9ae1e053a8c86c908b9a79b2a1e701; ZCCSUID=0e66b9ea49179f6184206ade6e573cb8; new_zacks_username=ehabwork0%40gmail.com; PHPSESSID=pro95qmok89hov4k3q36hnomk7; recentQuotes=CARG%2C; s_p42=premium%3A%20esp-buy; s_vnum=1614546000081%26vn%3D8; s_invisit=true; undefined_s=First%20Visit; _gat=1; AMCV_3064401053DB594D0A490D4C%40AdobeOrg=77933605%7CMCIDTS%7C18680%7CMCMID%7C02934501772713127552405615122030239818%7CMCAAMLH-1614554140%7C6%7CMCAAMB-1614554140%7CRKhpRz8krg2tLO6pguXWp5olkAcUniQYPHaMWWgdJ3xzPWQmdj0y%7CMCOPTOUT-1613956540s%7CNONE%7CMCAID%7CNONE%7CMCSYNCSOP%7C411-18687%7CvVersion%7C4.5.1; s_nr=1613949340919-Repeat; s_gapv=Logged%20In; s_sq=%5B%5BB%5D%5D; FCCDCF=[["AKsRol8HXgVqpjI9ifEq9bn_JG45jGlYf4j0oc08HmMcarT0vtpsXwdb1N2EBOTfSyTdC3Jsn6sXLqvD3Jx2lqk2fq_Cw-gx1V1QmWodNnXcz1d-n1p4YrpUWzRNWtJLTVE5dclZVkOHrSARId-f079s6ZFZ8f1CZQ=="],null,["[[],[],[],[],null,null,true]",1613949341347],["CPB0msuPB0msuEsABBENBNCoAP_AAG_AACiQF9JD7T5FYSFCwOpZVJgAEAgTQECAAiQAAAQAAmABQAIQAAACkkAQEASgAAAAAAAAAABBAQAMCAAACQABwABAAAAAAAAABAAAIAAAgAEAAAAIAAACAIAAAAAIAAAAEAAAiwL6gRAALAAeABUAC4AGQAOAAgABEACoAGgAOQAeQBDAEUAJgATwApABWACwAF8AQgApQBowDUANUAewBEQCLAEcAMVAdIB1ADyAKRAU2AvMBfQAAA","1~1765.1716.1577.2202.574.66.70.2357.1301.1451.93.864.2526.571.108.122.1878.440.1097.2253.167.2571.2373.317.311.184.1276.817.2575.196.89.1365.2072.1201.241.149.338.253.588.259.1236.1211.1095.2568.1651.1449.1570.1205.1051.2531.2299.1171.162.1415.415.1126.1127.1870.448.449.1753.486.495.494.482.2677.981.1889.323","170B9EB7-5F57-4A81-8D1B-B25CB7028139"],null]',
+    'Host': 'www.zacks.com',
+    'Origin': 'https://www.zacks.com',
+    'Referer': 'https://www.zacks.com/premium/esp-buy?adid=zp_prempage_beatearnings_espfilter&amp;icid=investment_services-zacks_premium-zp_internal-zacks_premium-top_stocks_to_beat_earnings-earnings_esp_filter',
+    'Sec-Fetch-Dest': 'empty',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Site': 'same-origin',
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36',
+    'X-Requested-With': 'XMLHttpRequest'
+}
